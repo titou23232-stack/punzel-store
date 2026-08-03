36ff01c517c3b6082395676c65dec815c461e115
noam ponchaut <punzelstor3@gmail.com>
2026-08-03 21:29:06 +0200

    update image

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ type Product = {
 type CartItem = Product & { quantity: number }
 
 const PRODUCTS: Product[] = [
-  { id: 1, name: 'Xanax 0,50mg', price: 15, image: 'https://via.placeholder.com/150', description: 'Description 1' },
+  { id: 1, name: 'Xanax 0,50mg', price: 15, image: 'https://mboapharma.cm/produit/alprazolam-biogaran-050-mg/', description: 'Description 1' },
   { id: 2, name: 'ordo', price: 35, image: 'https://via.placeholder.com/150', description: 'Description 2' },
   { id: 3, name: 'dextrometrophane', price: 20, image: 'https://via.placeholder.com/150', description: 'Description 3' }
 ]
