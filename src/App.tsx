a7eb50f0fb060ecb1e5c6b40a44445bfc52fb4c0
noam ponchaut <punzelstor3@gmail.com>
2026-08-05 15:55:45 +0200

    Roue avec nouvelle image finale

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const ADMIN_IDS = [5553381196, 6044402765]
 const VINTED_URL = 'https://www.vinted.fr/member/3164609498-punslestore5'
 const PAYPAL_URL = 'https://paypal.me/PunzelStore'
 
-const WHEEL_IMAGE = 'https://i.imgur.com/x5DcBt6.jpeg'
+const WHEEL_IMAGE = 'https://i.imgur.com/fxIuB8y.jpeg'
 
 type Product = {
   id: number
